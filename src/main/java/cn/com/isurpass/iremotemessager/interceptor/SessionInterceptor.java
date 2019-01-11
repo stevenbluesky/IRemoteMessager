1e6d797447c3cd512554cd9d2a968987f6d76af4
zhul <zhul3i@live.com>
2019-01-11 11:13:13 +0800

    /version 接口不做拦截

--- src/main/java/cn/com/isurpass/iremotemessager/interceptor/SessionInterceptor.java
+++ src/main/java/cn/com/isurpass/iremotemessager/interceptor/SessionInterceptor.java
@@ -5,12 +5,20 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashSet;
 
 public class SessionInterceptor implements HandlerInterceptor {
+    private static final HashSet<String> EXCEPT_URI = new HashSet();
+    static {
+        EXCEPT_URI.add("/login");
+        EXCEPT_URI.add("/checklogin");
+        EXCEPT_URI.add("/version");
+    }
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object o)throws Exception{
         //登录不做拦截
-        if(request.getRequestURI().equals(request.getContextPath()+"/login")||request.getRequestURI().equals(request.getContextPath()+"/checklogin")){
+        if(EXCEPT_URI.contains(request.getRequestURI())){
             return true;
         }
         //验证
